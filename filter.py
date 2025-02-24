a29e18dbe941b7dbc5f309a393f6ec0b92b207ab
Thomas <thomaslaene@gmail.com>
2025-02-24 23:37:29 +0100

    f

--- filter.py
+++ filter.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("adoveo20250214.csv", delimiter=";", skiprows=2)
 
-target_keywords = "Swisha en hälsning idag på Alla hjärtans dag så får mottagaren du väljer ett fint gåvokort med din personliga hälsning via SMS. En omtanke för någon du bryr dig om – och för djuren."
+keywords = "Swisha en hälsning idag på Alla hjärtans dag så får mottagaren du väljer ett fint gåvokort med din personliga hälsning via SMS. En omtanke för någon du bryr dig om – och för djuren."
 
 filtered_df = df[df["from"] == "DjurensRatt"]
-filtered_df = df[df["message"].str.contains(target_keywords, case=False, na=False)]
+filtered_df = df[df["message"].str.contains(keywords, case=False, na=False)]
 
 # Save to a new CSV file
 filtered_df.to_csv("filtered_messages.csv", index=False)
